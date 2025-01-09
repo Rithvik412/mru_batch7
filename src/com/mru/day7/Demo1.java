a8166bfc3d9fbf0f95364500d36bdeed89e491a3
Rithvik412 <2311cs010412@mallareddyuniversity.ac.in>
2025-01-09 11:49:43 +0530

    8 line code

--- src/com/mru/day7/Demo1.java
+++ src/com/mru/day7/Demo1.java
@@ -5,7 +5,7 @@ public class Demo1 {
 	public static void main(String[] args) {
 		System.out.println("10 line code");
 		System.out.println("20 line code");
-
+		System.out.println("30 line code");
 	}
 
 }
